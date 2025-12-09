45b0fc007f91fbfcf4d888104be6e1f6c709d45b
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-12-09 19:00:48 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
-        "http://localhost:8000"
+        "http://localhost:8000",
+        "https://hackthon-project-dusky.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
